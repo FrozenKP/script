2852fa82965ec73c4e74ca24c2ce48896fa65f25
FrozenKP <csjh21010@gmail.com>
2016-06-18 23:57:06 +0800

    Trackpad can't tap

---
+++ tap.cpp
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  tap.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  西元2016年06月18日 23時49分34秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+	system("synclient TapButton1=1");
+	return 0;
+}
